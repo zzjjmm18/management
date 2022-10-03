d69eaa527a8ee029d25f8180fa95affae37f5e2b
jm <1638718049@qq.com>
2022-10-03 18:04:52 +0800

    角色管理和公司信息模块完成

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,5 +4,7 @@ const getters = {
   token: state => state.user.token, // 设置token的映射
   name: state => state.user.userInfo.username, // 设置用户名称的映射
   staffPhoto: state => state.user.userInfo.staffPhoto // 设置用户头像的映射
+  // companyId: state => state.user.userInfo.companyId // 设置companyId的映射
+
 }
 export default getters
