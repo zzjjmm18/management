506ad53d7f18b8e897d6bc58a1ac12ddd135efef
jm <1638718049@qq.com>
2022-10-04 15:38:39 +0800

    审批模块完成

--- src/router/modules/approvals.js
+++ src/router/modules/approvals.js
@@ -1,16 +1,72 @@
 import Layout from '@/layout'
+
 export default {
-  path: '/approvals', // 路径
-  name: 'approvals', // 给路由规则加一个name
-  component: Layout, // 组件
-  // 配置二级路的路由表
-  children: [{
-    path: '', // 二级路由的默认路由
-    component: () => import('@/views/approvals'),
-    // 路由元信息  其实就是存储数据的对象
-    meta: {
-      title: '审批',
-      icon: 'tree-table'
+  path: '/approvals',
+  component: Layout,
+  name: 'approvals',
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/approvals'),
+      name: 'approvals',
+      meta: {
+        title: '审批',
+        icon: 'tree-table'
+      }
+    },
+    {
+      path: 'salaryApproval/:id',
+      component: () => import('@/views/approvals/salary'),
+      name: 'salaryApproval',
+      hidden: true,
+      meta: {
+        title: '工资审核',
+        icon: 'approval', noCache: true }
+    },
+    {
+      path: 'enterApproval/:id',
+      component: () => import('@/views/approvals/enter'),
+      name: 'enterApproval',
+      hidden: true,
+      meta: {
+        title: '入职审核',
+        icon: 'approval', noCache: true }
+    },
+    {
+      path: 'leaveApproval/:id',
+      component: () => import('@/views/approvals/leave'),
+      name: 'leaveApproval',
+      hidden: true,
+      meta: {
+        title: '申请请假',
+        icon: 'approval', noCache: true }
+    },
+    {
+      path: 'quitApproval/:id',
+      component: () => import('@/views/approvals/quit'),
+      name: 'quitApproval',
+      hidden: true,
+      meta: {
+        title: '申请离职',
+        icon: 'approval', noCache: true }
+    },
+    {
+      path: 'overtimeApproval/:id',
+      component: () => import('@/views/approvals/overtime'),
+      name: 'overtimeApproval',
+      hidden: true,
+      meta: {
+        title: '加班申请',
+        icon: 'approval', noCache: true }
+    },
+    {
+      path: 'securitySetting',
+      component: () => import('@/views/approvals/security'),
+      name: 'securitySetting',
+      hidden: true,
+      meta: {
+        title: '设置',
+        icon: 'approval', noCache: true }
     }
-  }]
+  ]
 }
