eedadba474170b8eeef3e4bb67bbd26282eb8c81
jm <1638718049@qq.com>
2022-10-05 10:46:43 +0800

    考勤模块完成

--- src/router/modules/attendances.js
+++ src/router/modules/attendances.js
@@ -1,16 +1,37 @@
+
 import Layout from '@/layout'
-export default {
-  path: '/attendances', // 路径
-  name: 'attendances', // 给路由规则加一个name
-  component: Layout, // 组件
-  // 配置二级路的路由表
-  children: [{
-    path: '', // 二级路由的默认路由
-    component: () => import('@/views/attendances'),
-    // 路由元信息  其实就是存储数据的对象
-    meta: {
-      title: '考勤',
-      icon: 'skill'
+
+const attendRouter = {
+  path: '/attendances',
+  component: Layout,
+  name: 'attendances',
+  children: [
+    {
+      path: '',
+      component: () => import('@/views/attendances'),
+      name: 'attendances',
+      meta: {
+        title: '考勤',
+        icon: 'excel' }
+    },
+    {
+      path: 'archiving',
+      component: () => import('@/views/attendances/historical'),
+      name: 'archiving',
+      hidden: true,
+      meta: {
+        title: '归档'
+      }
+    },
+    {
+      path: 'report/:month',
+      component: () => import('@/views/attendances/report'),
+      name: 'reports',
+      hidden: true,
+      meta: {
+        title: '报表'
+      }
     }
-  }]
+  ]
 }
+export default attendRouter
